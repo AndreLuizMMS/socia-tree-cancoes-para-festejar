63e16dce447aeeaf9b35e2686f6711dda211adf5
Andre Luiz Morais <andre.luiz@setrasolucoes.com.br>
2024-09-21 13:51:21 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="outside">
+    <div class="wrapper">
+      <div class="main-card">
+        <div class="header">
+          <img src="./assets/PHOTO-2024-05-08-19-24-49.jpg" class="profile-image" />
+          <span> Canções para Festejar </span>
+        </div>
+
+        <div class="info">
+          <button-redirect />
+        </div>
+
+        <div class="fotter">
+          <img src="./assets/log_cancoes.png" class="logo" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonRedirect from './components/ButtonRedirect.vue';
+
+export default {
+  components: { ButtonRedirect },
+  data() {
+    return {
+      buttons: [
+        {
+          text: 'Grupo Vip',
+          image: './assets/grupo_vip.jpg',
+          link: ''
+        },
+        {
+          text: '',
+          image: './assets/Aula_presencial.png',
+          link: ''
+        },
+        {
+          text: '',
+          image: './assets/Aula_online.png',
+          link: ''
+        },
+        {
+          text: '',
+          image: '',
+          link: ''
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import url(https://db.onlinewebfonts.com/c/628e4b3a4d1bbe699a9948f5aa0de8cf?family=Recta+W01+Light+Condensed);
+* {
+  // border: black 1px solid;
+  margin: 0;
+  font-family: Arial Black,Arial Bold,Gadget,sans-serif; 
+}
+
+.outside {
+  background-color: #415291;
+  // height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.wrapper {
+  background-color: #415291;
+  margin-block: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.main-card {
+  padding-block: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 23rem;
+  border: #bfaa9e solid 3px;
+  border-radius: 5px;
+  box-shadow: #aa9584 1px 1px 10px;
+  height: 100%;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 5px 5px 0 0;
+
+  span {
+    color: white;
+    letter-spacing: 3px;
+    font-size: 1.6rem;
+    margin-bottom: 2rem;
+  }
+
+  .profile-image {
+    // margin-top: 5px;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    object-fit: cover;
+    border: 3px solid #bfaa9e;
+  }
+}
+
+.info {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 5px;
+  width: 90%;
+}
+
+.logo {
+  margin-top: 9px;
+  width: 10rem;
+}
+</style>
---
+++ src/components/ButtonRedirect.vue
@@ -0,0 +1,92 @@
+<template>
+  <a :href="urlRedirect" class="link-wrapper">
+    <div class="button-wrapper">
+      <div class="image-container">
+        <img src="../assets/grupo_vip.jpg" alt="button image" />
+      </div>
+      <div class="info-container">
+        <span>Grupo Vip</span>
+      </div>
+    </div>
+  </a>
+
+  <a :href="urlRedirect" class="link-wrapper">
+    <div class="button-wrapper">
+      <div class="image-container">
+        <img src="../assets/Aula_presencial.png" alt="button image" />
+      </div>
+      <div class="info-container">
+        <span>Agende sua Aula Presencial</span>
+      </div>
+    </div>
+  </a>
+
+  <a :href="urlRedirect" class="link-wrapper">
+    <div class="button-wrapper">
+      <div class="image-container">
+        <img src="../assets/Aula_online.png" alt="button image" />
+      </div>
+      <div class="info-container">
+        <span>Agende sua Aula On-line 🎶</span>
+      </div>
+    </div>
+  </a>
+
+  <a :href="urlRedirect" class="link-wrapper">
+    <div class="button-wrapper">
+      <div class="image-container">
+        <img src="../assets/log_cancoes.png" alt="button image" />
+      </div>
+      <div class="info-container">
+        <span>Orçamento Eventos </span>
+        <br />
+        <span>Canções para celebrar</span>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    urlRedirect: String,
+    imagePath: String
+  },
+  mounted() {
+    console.log('Image Path:', this.imagePath);
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.link-wrapper {
+  width: 100%;
+  border: #bfaa9e solid 3px;
+  border-radius: 5px;
+  box-shadow: #aa9584 1px 1px 10px;
+  // background-color: #415291;
+  
+}
+
+.button-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: baseline;
+  width: 100%;
+  height: auto;
+  gap: 0.5rem;
+  padding: 5px;
+}
+
+span {
+  color: white;
+  font-size: 1.2rem;
+  letter-spacing: 1px;
+}
+
+.image-container img {
+  width: 5rem;
+  border-radius: 5px;
+}
+</style>
